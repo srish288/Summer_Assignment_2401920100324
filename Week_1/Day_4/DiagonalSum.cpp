0c76156afe8d6268f89e49d835cb97d9b233ba0c
Srishti Sharma <sriiieeee@gmail.com>
2026-06-10 20:28:48 +0530

    adding updted solution

--- Week_1/Day_4/DiagonalSum.cpp
+++ Week_1/Day_4/DiagonalSum.cpp
@@ -4,12 +4,13 @@ public:
         int n = mat.size();
         int sum = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j || i + j == n - 1) {
-                    sum += mat[i][j];
-                }
-            }
+        for(int i = 0; i < n; i++) {
+            sum += mat[i][i];
+            sum += mat[i][n - 1 - i];
+        }
+
+        if(n % 2 == 1) {
+            sum -= mat[n/2][n/2];
         }
 
         return sum;
